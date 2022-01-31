772b26c6e8d5bf64393713043780cc78370e708c
Christos Ferderigos <ferderigosc@gmail.com>
2022-01-31 02:44:44 -0500

    Database connection now uses environment variables

--- src/configurations/database.js
+++ src/configurations/database.js
@@ -5,25 +5,13 @@ const {
    POSTGRES_PASSWORD, 
    POSTGRES_DB } = require("./credentials");
 
-// module.exports = db = require('knex')({
-//   client: "pg" ,
-//   connection: {
-//     host : POSTGRES_HOST,
-//     port : POSTGRES_PORT,
-//     user : POSTGRES_USER,
-//     password : POSTGRES_PASSWORD,
-//     database : POSTGRES_DB
-//   }
-// });
-
-
 module.exports = db = require('knex')({
   client: "pg" ,
   connection: {
-    host : "db",
-    port : 5432,
-    user : "postgres",
-    password : "example",
-    database : "covid_db"
+    host : POSTGRES_HOST,
+    port : POSTGRES_PORT,
+    user : POSTGRES_USER,
+    password : POSTGRES_PASSWORD,
+    database : POSTGRES_DB
   }
 });
